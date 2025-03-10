d9f4e25e93913fd9c16498a3573deed084d79c88
AskarTech <ahmedasker359@gmail.com>
2025-03-11 01:05:06 +0300

    Implement note deletion functionality: add POST method handling and delete button in the note view

--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -11,6 +11,11 @@
             <a href="/notes" class="text-blue-500 underline">go back...</a>
         </p>
         <p><?= $note['body'] ?></p>
+
+        <form class="mt-6" method="POST">
+            <input type="hidden" name="id" value="<?= $note['id'] ?>">
+            <button class="text-sm text-red-500">Delete</button>
+        </form>
     </div>
 </main>
 <?php require base_path('views/partials/footer.php') ?>
